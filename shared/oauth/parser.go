6de5371919c3a09f44ecd5a8a357bc65b6f4c59e
Yudha Subki <yudhasubki@gmail.com>
2020-11-12 09:45:48 +0700

    add validation empty token (#53)
    * Update: add validation empty token
    
    * Update: check token empty
    
    Co-authored-by: Yudha Pratama Putra <yudha.pratama@salestock.id>

--- shared/oauth/parser.go
+++ shared/oauth/parser.go
@@ -16,9 +16,15 @@ func NewParser(tokenStore TokenStore) *Parser {
 }
 
 func (p *Parser) Parse(accessToken string) (accessTokenClient OauthAccessToken, err error) {
+	valid := p.validToken(accessToken)
+	if !valid {
+		err = errors.New(ErrorEmptyCredential)
+		return
+	}
+
 	token := strings.Split(accessToken, " ")
 
-	if !p.validTokenTypeBearer(token[0]) {
+	if !p.validTokenTypeBearer(token[0]) || len(token) == 1 {
 		err = errors.New(ErrorTokenTypeMismatch)
 		return
 	}
@@ -33,6 +39,14 @@ func (p *Parser) Parse(accessToken string) (accessTokenClient OauthAccessToken, 
 
 func (p *Parser) validTokenTypeBearer(tokenType string) bool {
 	if tokenType != string(Bearer) {
+		return false
+	}
+
+	return true
+}
+
+func (p *Parser) validToken(accessToken string) bool {
+	if accessToken == "" {
 		return false
 	}
 
